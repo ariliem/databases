d75e9482f507cee633ed5d819707cbeae99b0fe7
ariliem <156829193+ariliem@users.noreply.github.com>
2024-09-30 21:48:14 +0500

    Create 1.sql

---
+++ lab3/1.sql
@@ -0,0 +1,130 @@
+--1
+create database lab3;
+
+--2
+create table departments (
+  code integer primary key,
+  name varchar(255) not null ,
+  budget decimal not null
+);
+
+create table employees (
+  ssn integer primary key,
+  name varchar(255) not null ,
+  lastname varchar(255) not null ,
+  department integer not null ,
+  city varchar(255),
+  foreign key (department) references departments(code)
+);
+
+create table customers (
+  id serial primary key,
+  name varchar(255) not null ,
+  lastname varchar(255) not null ,
+  city varchar(255)
+);
+
+INSERT INTO departments(code,name,budget) VALUES(14,'IT',65000);
+INSERT INTO departments(code,name,budget) VALUES(37,'Accounting',15000);
+INSERT INTO departments(code,name,budget) VALUES(59,'Human Resources',240000);
+INSERT INTO departments(code,name,budget) VALUES(77,'Research',55000);
+INSERT INTO departments(code,name,budget) VALUES(45,'Management',155000);
+INSERT INTO departments(code,name,budget) VALUES(11,'Sales',85000);
+
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('123234877','Michael','Rogers',14, 'Almaty');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('152934485','Anand','Manikutty',14, 'Shymkent');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('222364883','Carol','Smith',37, 'Astana');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('326587417','Joe','Stevens',37, 'Almaty');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('332154719','Mary-Anne','Foster',14, 'Astana');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('332569843','George','ODonnell',77, 'Astana');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('546523478','John','Doe',59, 'Shymkent');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('631231482','David','Smith',77, 'Almaty');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('654873219','Zacary','Efron',59, 'Almaty');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('745685214','Eric','Goldsmith',59, 'Atyrau');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('845657245','Elizabeth','Doe',14, 'Almaty');
+INSERT INTO employees(ssn,name,lastname,department, city) VALUES('845657246','Kumar','Swamy',14, 'Almaty');
+
+
+
+INSERT INTO customers(name,lastname, city) VALUES('John','Wills', 'Almaty');
+INSERT INTO customers(name,lastname, city) VALUES('Garry','Foster', 'London');
+INSERT INTO customers(name,lastname, city) VALUES('Amanda','Hills', 'Almaty');
+INSERT INTO customers(name,lastname, city) VALUES('George','Doe', 'Tokyo');
+INSERT INTO customers(name,lastname, city) VALUES('David','Little', 'Almaty');
+INSERT INTO customers(name,lastname, city) VALUES('Shawn','Efron', 'Astana');
+INSERT INTO customers(name,lastname, city) VALUES('Eric','Gomez', 'Shymkent');
+INSERT INTO customers(name,lastname, city) VALUES('Elizabeth','Tailor', 'Almaty');
+INSERT INTO customers(name,lastname, city) VALUES('Julia','Adams', 'Astana');
+
+--3
+select lastname from employees;
+
+--4
+select distinct lastname from employees;
+
+--5
+select * from employees
+where lastname = 'Smith';
+
+--6
+select * from employees
+where lastname in('Smith', 'Doe');
+
+--7
+select * from employees
+where department = 14;
+
+--8
+select * from employees
+where department in(37,77);
+
+--9
+select sum(budget) from departments;
+
+--10
+select department, count(department) from employees group by department;
+
+--11
+select department, count(department) from employees
+group by department
+having count(department) > 2;
+
+--12
+select name from departments
+order by budget desc
+limit 1 offset 1;
+
+--13
+select name, lastname
+from employees
+where department = (select code from departments order by budget limit 1);
+
+--14
+select name from customers where city = 'Almaty'
+union
+select name from employees where city = 'Almaty';
+
+--15
+select * from departments where budget > 60000
+order by budget asc, code desc;
+
+--16
+update departments
+set budget = budget*0.9
+where code = (select code from departments order by budget asc limit 1);
+
+select * from departments;
+
+--17
+update employees
+set department = (select code from departments where name = 'IT')
+where department = (select code from departments where name = 'Research');
+
+select * from employees;
+
+--18
+delete from employees
+where department = (select code from departments where name = 'IT');
+
+--19
+delete from employees;
